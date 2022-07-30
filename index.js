b1dd498cc08c3eb78b6c4ce145ad6890d04a2591
Anthony Abou Khalil <72323137+anthonyaboukhalil@users.noreply.github.com>
2022-07-29 17:27:35 -0700

    heroku

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   // if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.redirect("/");
+    res.sendFile(path.resolve(__dirname, "client", "build/index.html"));
   });
 }
 const PORT = process.env.PORT || 5000;
